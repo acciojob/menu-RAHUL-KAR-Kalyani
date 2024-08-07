3dc0bc21ebf4c83e22a52dace0ebd3c135c2e0c1
RK <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2024-08-07 14:41:12 +0530

    updated

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,21 +1,10 @@
-// cypress/integration/dishList.spec.js
-
 describe('DishList Component', () => {
     beforeEach(() => {
-      cy.visit('/'); // Adjust if your app is hosted at a different URL
+      cy.visit('/'); // This should be the root of your app or adjust as necessary
     });
   
     it('should render DishList correctly', () => {
       cy.get('.dish-list').should('exist'); // Check if DishList is rendered
     });
-  
-    it('should display items belonging to category Shakes', () => {
-      cy.get('[data-testid="category-shakes"]').click(); // Simulate category click
-      cy.get('.dish-card').each($dishCard => {
-        cy.wrap($dishCard).should('contain', 'shake'); // Adjust as per your data
-      });
-    });
-  
-    // Add similar tests for other categories
   });
   
