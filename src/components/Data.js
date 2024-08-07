0a857ab0388e0577c44130000f0cc36120f1b018
RK <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2024-08-07 14:37:58 +0530

    updated

--- src/components/Data.js
+++ src/components/Data.js
@@ -5,7 +5,7 @@ export const dishes = [
         title: 'buttermilk pancakes',
         category: 'breakfast',
         price: 15.99,
-        img: 'https://img.freepik.com/free-photo/ai-generated-image-banana_23-2150683179.jpg?t=st=1721956938~exp=1721960538~hmac=b72207cd6adef8b2b84e4762c6eab22eb221447277470ff76fd5fcc8c0cb1a7f&w=740',
+        img: './assets/img1.jpg',
         desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
     {
@@ -13,7 +13,7 @@ export const dishes = [
         title: 'diner double',
         category: 'lunch',
         price: 13.99,
-        img: 'https://img.freepik.com/free-photo/ai-generated-image-banana_23-2150683179.jpg?t=st=1721956938~exp=1721960538~hmac=b72207cd6adef8b2b84e4762c6eab22eb221447277470ff76fd5fcc8c0cb1a7f&w=740',
+        img: './assets/img2.jpg',
         desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
     },
     {
@@ -21,7 +21,7 @@ export const dishes = [
         title: 'godzilla milkshake',
         category: 'shakes',
         price: 6.99,
-        img: 'https://img.freepik.com/free-photo/ai-generated-image-banana_23-2150683179.jpg?t=st=1721956938~exp=1721960538~hmac=b72207cd6adef8b2b84e4762c6eab22eb221447277470ff76fd5fcc8c0cb1a7f&w=740',
+        img: './assets/img3.jpg',
         desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
     },
     {
@@ -29,7 +29,7 @@ export const dishes = [
         title: 'country delight',
         category: 'breakfast',
         price: 20.99,
-        img: 'https://img.freepik.com/free-photo/ai-generated-image-banana_23-2150683179.jpg?t=st=1721956938~exp=1721960538~hmac=b72207cd6adef8b2b84e4762c6eab22eb221447277470ff76fd5fcc8c0cb1a7f&w=740',
+        img: './assets/img4.jpg',
         desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
     },
     {
@@ -37,7 +37,7 @@ export const dishes = [
         title: 'egg attack',
         category: 'lunch',
         price: 22.99,
-        img: 'https://img.freepik.com/free-photo/ai-generated-image-banana_23-2150683179.jpg?t=st=1721956938~exp=1721960538~hmac=b72207cd6adef8b2b84e4762c6eab22eb221447277470ff76fd5fcc8c0cb1a7f&w=740',
+        img: './assets/img5.jpg',
         desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
     },
     {
@@ -45,7 +45,7 @@ export const dishes = [
         title: 'oreo dream',
         category: 'shakes',
         price: 18.99,
-        img: 'https://img.freepik.com/free-photo/ai-generated-image-banana_23-2150683179.jpg?t=st=1721956938~exp=1721960538~hmac=b72207cd6adef8b2b84e4762c6eab22eb221447277470ff76fd5fcc8c0cb1a7f&w=740',
+        img: './assets/img6.jpg',
         desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
     },
     {
@@ -53,7 +53,7 @@ export const dishes = [
         title: 'bacon overflow',
         category: 'breakfast',
         price: 8.99,
-        img: 'https://img.freepik.com/free-photo/ai-generated-image-banana_23-2150683179.jpg?t=st=1721956938~exp=1721960538~hmac=b72207cd6adef8b2b84e4762c6eab22eb221447277470ff76fd5fcc8c0cb1a7f&w=740',
+        img: './assets/img7.jpg',
         desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
     },
     {
@@ -61,7 +61,7 @@ export const dishes = [
         title: 'american classic',
         category: 'lunch',
         price: 12.99,
-        img: 'https://img.freepik.com/free-photo/ai-generated-image-banana_23-2150683179.jpg?t=st=1721956938~exp=1721960538~hmac=b72207cd6adef8b2b84e4762c6eab22eb221447277470ff76fd5fcc8c0cb1a7f&w=740',
+        img: './assets/img8.jpg',
         desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
     },
     {
@@ -69,7 +69,7 @@ export const dishes = [
         title: 'quarantine buddy',
         category: 'shakes',
         price: 16.99,
-        img: 'https://img.freepik.com/free-photo/ai-generated-image-banana_23-2150683179.jpg?t=st=1721956938~exp=1721960538~hmac=b72207cd6adef8b2b84e4762c6eab22eb221447277470ff76fd5fcc8c0cb1a7f&w=740',
+        img: './assets/img9.jpg',
         desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
 ]
--- src/components/DishCard.js
+++ src/components/DishCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DishCard = ({ dish }) => {
   return (
     <div className="dish-card">
-      <img src={dish.img} alt={dish.title} />
+      <img src={dish.img} alt={dish.title} height={200} width={200}/>
       <h3>{dish.title}</h3>
       <p>{dish.desc}</p>
       <p>Price: ${dish.price.toFixed(2)}</p>
